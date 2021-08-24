89c66d7ed8b6e8d99fd161b847a8fe13e6200b97
Kelsey Thompson <kelsthompson2@gmail.com>
2021-08-24 17:45:30 -0600

    Complete migration and add model files for ride and volunteer

--- db/migrate/20210824221334_create_rides.rb
+++ db/migrate/20210824221334_create_rides.rb
@@ -4,7 +4,6 @@ class CreateRides < ActiveRecord::Migration[5.2]
       t.string :name
       t.boolean :roller_coaster
       t.integer :top_speed
-      t.references :amusementpark, foreign_key: true
       t.timestamps
     end
   end
