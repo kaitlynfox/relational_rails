160901a49f56528b89f3f6ce67039e4872f41d5b
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-08-25 12:09:46 -0600

    Add welcome and amusement_parks routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Welcome
+  get '/', to: 'welcome#index'
+
+  # AmusementPark
+  get '/amusement_parks', to: 'amusement_parks#index'
+  # Ride
+  # Zoo
+    get '/zoos', to: 'zoos#index'
+  # Volunteer
 end
