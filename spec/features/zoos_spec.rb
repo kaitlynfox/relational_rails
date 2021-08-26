b000c8409b1720074ea71f099193a0bbc674586c
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-08-25 19:39:33 -0600

    Create view

--- spec/features/zoos_spec.rb
+++ spec/features/zoos_spec.rb
@@ -1,14 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe 'the zoos index page', type: :feature do
+RSpec.describe 'the zoos index page' do
   it "can see all zoos names" do
     zoo = Zoo.create!(name: "Denver City Zoo",
                       rank: 3,
                       state_funding: true,
                       city: "Denver")
 
-  visit '/zoos'
+    visit '/zoos'
+
+    expect(page).to have_content(zoo.name)
+  end
+
+  it "can see a zoo with the id and attributes" do
+    zoo = Zoo.create!(name: "Denver City Zoo",
+                      rank: 3,
+                      state_funding: true,
+                      city: "Denver")
+
+  visit "/zoos/#{zoo.id}"
 
   expect(page).to have_content(zoo.name)
+  expect(page).to have_content(zoo.rank)
+  expect(page).to have_content(zoo.state_funding)
+  expect(page).to have_content(zoo.city)
   end
 end
