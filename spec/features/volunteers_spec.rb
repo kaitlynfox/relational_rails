ff2a550125116e901b826c2b856c0c48eeabb2a4
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-08-27 17:11:37 -0600

    Add user stories

--- spec/features/volunteers_spec.rb
+++ spec/features/volunteers_spec.rb
@@ -12,26 +12,36 @@ RSpec.describe 'the volunteers index page' do
                                   certified_trainer: true,
                                   years_of_experience: 4,)
 
+    volunteer_2 = zoo.volunteers.create!(name: "Dale",
+                                  certified_trainer: true,
+                                  years_of_experience: 4,)
+
     visit '/volunteers'
+
+    save_and_open_page
 
     expect(page).to have_content(volunteer.name)
     expect(page).to have_content(volunteer.certified_trainer)
     expect(page).to have_content(volunteer.years_of_experience)
   end
 
-  xit "can see all amusement park names and its attributes" do
+  it "can see all amusement park names and its attributes" do
     zoo = Zoo.create!(name: "Denver City Zoo",
                       rank: 3,
                       state_funding: true,
                       city: "Denver")
 
-    volunteer = zoo.volunteers.create!(name: "Karen Dale",
+    volunteer = Volunteer.create!(name: "Karen Dale",
                                   certified_trainer: true,
-                                  years_of_experience: 4,)
+                                  years_of_experience: 4,
+                                  zoo_id: zoo.id)
 
-    visit "/volunteer/#{volunteer.id}"
+    visit "/volunteers/#{volunteer.id}"
+
+    save_and_open_page
 
     expect(page).to have_content(volunteer.name)
-
+    expect(page).to have_content(volunteer.certified_trainer)
+    expect(page).to have_content(volunteer.years_of_experience)
   end
 end
