1643c146cd8bc6c3c1651a471171a6e6d7c65452
Kelsey Thompson <kelsthompson2@gmail.com>
2021-08-29 08:46:01 -0600

    Write tests and implementation code for user stories 9-10

--- spec/features/amusement_parks_spec.rb
+++ spec/features/amusement_parks_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'the amusement parks index page' do
+RSpec.describe 'AmusmentPark' do
   it "can see all amusement park names" do
 
     amusement_park = AmusementPark.create!(name: "Walt Disney World",
@@ -69,7 +69,7 @@ RSpec.describe 'the amusement parks index page' do
     end
   end
 
-  describe 'user story 8' do
+  describe 'user story 9' do
     it "should have link to Rides index on all pages" do
       amusement_park = AmusementPark.create!(name: "Walt Disney World",
                                              cost_of_entry: 95,
@@ -87,27 +87,48 @@ RSpec.describe 'the amusement parks index page' do
                                           top_speed: 15,)
       # visit "/"
       # page.has_link?(true)
-      # page.click_link("Rides")
+      # page.click_link("Amusement Parks")
 
       visit "/amusement_parks"
       page.has_link?(true)
-      page.click_link("Rides")
+      page.click_link("Amusement Parks")
 
       visit "/amusement_parks/#{amusement_park.id}"
       page.has_link?(true)
-      page.click_link("Rides")
+      page.click_link("Amusement Parks")
 
       visit "/rides"
       page.has_link?(true)
-      page.click_link("Rides")
+      page.click_link("Amusement Parks")
 
       visit "/rides/#{ride.id}"
       page.has_link?(true)
-      page.click_link("Rides")
+      page.click_link("Amusement Parks")
 
       visit "/amusement_parks/#{amusement_park.id}/rides"
       page.has_link?(true)
-      page.click_link("Rides")
+      page.click_link("Amusement Parks")
+    end
+  end
+  describe 'user story 10' do
+    it "should have link to Rides by amusment park on parent show page" do
+      amusement_park = AmusementPark.create!(name: "Walt Disney World",
+                                             cost_of_entry: 95,
+                                             open_year_round: true,)
+      amusement_park_2 = AmusementPark.create!(name: "King's Island", cost_of_entry: 45, open_year_round: false)
+
+      ride = amusement_park_2.rides.create!(name: "The Beast",
+                                          roller_coaster: true,
+                                          top_speed: 55,)
+      ride_2 = amusement_park_2.rides.create!(name: "Diamond Back",
+                                          roller_coaster: true,
+                                          top_speed: 100,)
+      ride_3 = amusement_park.rides.create!(name: "Dumbo",
+                                          roller_coaster: false,
+                                          top_speed: 15,)
+      visit "/amusement_parks/#{amusement_park.id}"
+      page.has_link?(true)
+      page.click_link("Rides by Amusement Park")
     end
   end
 end
--- spec/features/rides_spec.rb
+++ spec/features/rides_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe 'Rides' do
                                         top_speed: 55,)
     ride_2 = amusement_park.rides.create!(name: "Diamond Back",
                                         roller_coaster: true,
-                                        top_speed: 100,)                    
+                                        top_speed: 100,)
 
     visit "/rides/#{ride.id}"
 
@@ -43,5 +43,46 @@ RSpec.describe 'Rides' do
     expect(page).to have_content(ride.name)
     expect(page).to have_content(ride.roller_coaster)
     expect(page).to have_content(ride.top_speed)
+  end
+  describe 'user story 8' do
+    it "should have link to Rides index on all pages" do
+      amusement_park = AmusementPark.create!(name: "Walt Disney World",
+                                             cost_of_entry: 95,
+                                             open_year_round: true,)
+      amusement_park_2 = AmusementPark.create!(name: "King's Island", cost_of_entry: 45, open_year_round: false)
+
+      ride = amusement_park_2.rides.create!(name: "The Beast",
+                                          roller_coaster: true,
+                                          top_speed: 55,)
+      ride_2 = amusement_park_2.rides.create!(name: "Diamond Back",
+                                          roller_coaster: true,
+                                          top_speed: 100,)
+      ride_3 = amusement_park.rides.create!(name: "Dumbo",
+                                          roller_coaster: false,
+                                          top_speed: 15,)
+      # visit "/"
+      # page.has_link?(true)
+      # page.click_link("Rides")
+
+      visit "/amusement_parks"
+      page.has_link?(true)
+      page.click_link("Rides")
+
+      visit "/amusement_parks/#{amusement_park.id}"
+      page.has_link?(true)
+      page.click_link("Rides")
+
+      visit "/rides"
+      page.has_link?(true)
+      page.click_link("Rides")
+
+      visit "/rides/#{ride.id}"
+      page.has_link?(true)
+      page.click_link("Rides")
+
+      visit "/amusement_parks/#{amusement_park.id}/rides"
+      page.has_link?(true)
+      page.click_link("Rides")
+    end
   end
 end
